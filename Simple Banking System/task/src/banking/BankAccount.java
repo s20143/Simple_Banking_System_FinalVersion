2dc182860327a43ca685e8592d856d54ee9427ea
Gaspa <s20143@pjwstk.edu.pl>
2021-08-30 20:48:14 +0200

    adding Luhn algorith

--- Simple Banking System/task/src/banking/BankAccount.java
+++ Simple Banking System/task/src/banking/BankAccount.java
@@ -1,6 +1,7 @@
 package banking;
 
 public class BankAccount extends Bank{
+
     private String cardNumber;
     private String pin;
     private int balance;
@@ -9,12 +10,25 @@ public class BankAccount extends Bank{
         balance = 0;
     }
     public void create(){
+        int counter = 0;
+
         for (int i = 0; i < 2 ; i++) {
             StringBuilder creatorOfNumbers = new StringBuilder();
             if(i == 0) {
                 creatorOfNumbers.append("400000");
-                for (int j = 0; j <10; j++) {
-                    creatorOfNumbers.append((int)(Math.random()*10));
+                for (int j = 7; j < 16; j++) {
+                    int numberToAppend = (int)(Math.random()*10);
+                    creatorOfNumbers.append(numberToAppend);
+                    if(j % 2 == 1)
+                        numberToAppend *= 2;
+                    if(numberToAppend > 9)
+                        numberToAppend -= 9;
+
+                    counter += numberToAppend;
+                }
+                for (int j = 0; j < 10 ; j++) {
+                    if((counter + j + 8) % 10 == 0)
+                        creatorOfNumbers.append(j);
                 }
                 cardNumber = creatorOfNumbers.toString();
             }else {
